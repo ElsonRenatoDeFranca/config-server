e7655062ac0c706c124c5d1f0a24d835d3a98e4a
e068635 <elson.franca@mastercard.com>
2019-06-21 19:28:53 -0300

    fixed issue with the server config

--- src/main/java/com/roommicroservices/serviceconfig/ConfigServerApplication.java
+++ src/main/java/com/roommicroservices/serviceconfig/ConfigServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class RoomMicroserviceConfigServerApplication {
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RoomMicroserviceConfigServerApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
 }
